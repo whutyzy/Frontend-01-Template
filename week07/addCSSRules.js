ea223e03a9db5afc7fdde1ea663ee454d9698130
whutyzy <yeziyang@qiyi.com>
2020-06-13 19:14:39 +0800

    code save

--- week07/addCSSRules.js
+++
@@ -1,9 +0,0 @@
-const css = require('css')
-
-let rules = []
-function addCSSRules(text) {
-    let ast = css.parse(text)
-    
-    rules.push(...ast.stylesheet.rules)
-}
-module.exports = addCSSRules
--- week07/computeCSS.js
+++
@@ -1,9 +0,0 @@
-const css = require('css')
-
-let rules = []
-function computeCSS(text) {
-    let ast = css.parse(text)
-
-    rules.push(...ast.stylesheet.rules)
-}
-module.exports = computeCSS
